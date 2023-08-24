a40cab92196cc9e420557b1c575e0f8747ae0ef4
levtus <61996958+alexandsr@users.noreply.github.com>
2023-08-24 13:18:11 +0200

    Add dependencies & Initialize Gradle

--- src/main/java/net/aaw/extendedseating/ExtendedSeating.java
+++ src/main/java/net/aaw/extendedseating/ExtendedSeating.java
@@ -1,20 +1,4 @@
-package net.aaw.bellsandwhistles;
-
-import com.mojang.logging.LogUtils;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+package net.aaw.extendedseating;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
@@ -30,12 +14,12 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(BellsAndWhistles.MOD_ID)
-public class BellsAndWhistles {
-    public static final String MOD_ID = "bellsandwhistles";
+@Mod(net.aaw.extendedseating.ExtendedSeating.MOD_ID)
+public class ExtendedSeating {
+    public static final String MOD_ID = "extendedseating";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public BellsAndWhistles() {
+    public ExtendedSeating() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
