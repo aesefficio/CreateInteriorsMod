b4a0753435413370b37bd5004c91385565d09fea
levtus <i@alexander.systems>
2023-09-27 19:02:48 -0500

    Fix broken loot tables

--- src/main/java/systems/alexander/interiors/block/ModBlocks.java
+++ src/main/java/systems/alexander/interiors/block/ModBlocks.java
@@ -1,7 +1,5 @@
 package systems.alexander.interiors.block;
 
-import com.simibubi.create.AllCreativeModeTabs;
-import com.simibubi.create.Create;
 import com.simibubi.create.content.contraptions.actors.seat.SeatInteractionBehaviour;
 import com.simibubi.create.content.contraptions.actors.seat.SeatMovementBehaviour;
 import com.simibubi.create.content.redstone.displayLink.source.EntityNameDisplaySource;
@@ -14,6 +12,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -23,6 +22,7 @@ import systems.alexander.interiors.block.custom.BigSeatMovementBehaviour;
 import systems.alexander.interiors.block.custom.ChairBlockExtendsSeat;
 import systems.alexander.interiors.block.custom.DirectionalSeatBlock;
 import systems.alexander.interiors.block.util.ModTags;
+import systems.alexander.interiors.item.ModCreativeModeTabs;
 import systems.alexander.interiors.item.ModItems;
 
 import java.util.function.Supplier;
@@ -34,9 +34,24 @@ import static com.simibubi.create.foundation.data.TagGen.axeOnly;
 import static systems.alexander.interiors.Interiors.REGISTRATE;
 
 public class ModBlocks {
-    static {
-        Create.REGISTRATE.creativeModeTab(() -> AllCreativeModeTabs.BASE_CREATIVE_TAB);
-    }
+    public static final DyedBlockList<ChairBlockExtendsSeat> CHAIRS = new DyedBlockList<>(colour -> {
+        String colourName = colour.getSerializedName();
+        BigSeatMovementBehaviour movementBehaviour = new BigSeatMovementBehaviour();
+        SeatInteractionBehaviour interactionBehaviour = new SeatInteractionBehaviour();
+        return REGISTRATE.block(colourName + "_chair", p -> new ChairBlockExtendsSeat(p, colour, true))
+                .initialProperties(SharedProperties::wooden)
+                .properties(p -> p.color(colour.getMaterialColor()))
+                .transform(axeOnly())
+                .onRegister(movementBehaviour(movementBehaviour))
+                .onRegister(interactionBehaviour(interactionBehaviour))
+                .onRegister(assignDataBehaviour(new EntityNameDisplaySource(), "entity_name"))
+                .tag(ModTags.Blocks.CHAIRS)
+                .item()
+                .tag(ModTags.Items.CHAIRS)
+                .build()
+                .loot((lt, block) -> lt.dropSelf(block))
+                .register();
+    });
 
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Interiors.MOD_ID);
@@ -48,45 +63,36 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> RED_CHAIR_ICON = registerBlock("red_chair_icon",
                      () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS).noOcclusion()));
-    public static final DyedBlockList<ChairBlockExtendsSeat> CHAIRS = new DyedBlockList<>(colour -> {
-        String colourName = colour.getSerializedName();
-        BigSeatMovementBehaviour movementBehaviour = new BigSeatMovementBehaviour();
-        SeatInteractionBehaviour interactionBehaviour = new SeatInteractionBehaviour();
-        return REGISTRATE.block(colourName + "_chair", p -> new ChairBlockExtendsSeat(p, colour, true))
-                .initialProperties(SharedProperties::wooden)
-                .transform(axeOnly())
-                .onRegister(movementBehaviour(movementBehaviour))
-                .onRegister(interactionBehaviour(interactionBehaviour))
-                .onRegister(assignDataBehaviour(new EntityNameDisplaySource(), "entity_name"))
-                .tag(ModTags.Blocks.CHAIRS)
-                .item()
-                .tag(ModTags.Items.CHAIRS)
-                .build()
-                .register();
-    });
-
     public static final RegistryEntry<ChairBlockExtendsSeat> KELP_CHAIR = REGISTRATE.block("kelp_chair", p -> new ChairBlockExtendsSeat(p, DyeColor.BLACK, true))
             .initialProperties(SharedProperties::wooden)
+            .properties(p -> p.color(MaterialColor.TERRACOTTA_BLACK))
             .transform(axeOnly())
             .onRegister(movementBehaviour(new BigSeatMovementBehaviour()))
             .onRegister(interactionBehaviour(new SeatInteractionBehaviour()))
             .simpleItem()
+            .loot((lt, block) -> lt.dropSelf(block))
             .register();
-
     public static final RegistryEntry<DirectionalSeatBlock> KELP_SEAT = REGISTRATE.block("kelp_seat", p -> new DirectionalSeatBlock(p, DyeColor.BLACK, true))
             .initialProperties(SharedProperties::wooden)
+            .properties(p -> p.color(MaterialColor.TERRACOTTA_BLACK))
             .transform(axeOnly())
             .onRegister(movementBehaviour(new SeatMovementBehaviour()))
             .onRegister(interactionBehaviour(new SeatInteractionBehaviour()))
             .onRegister(assignDataBehaviour(new EntityNameDisplaySource(), "entity_name"))
             .simpleItem()
+            .loot((lt, block) -> lt.dropSelf(block))
             .register();
-
     public static final RegistryEntry<Block> SEATWOOD_PLANKS = REGISTRATE.block("seatwood_planks", p -> new Block(p))
             .initialProperties(SharedProperties::wooden)
+            .properties(p -> p.color(MaterialColor.WOOD))
             .transform(axeOnly())
             .simpleItem()
+            .loot((lt, block) -> lt.dropSelf(block))
             .register();
+
+    static {
+        Interiors.REGISTRATE.creativeModeTab(() -> ModCreativeModeTabs.INTERIORS_TAB);
+    }
     private static <T extends Block>RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
         return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     };
--- src/main/java/systems/alexander/interiors/item/ModCreativeModeTabs.java
+++ src/main/java/systems/alexander/interiors/item/ModCreativeModeTabs.java
@@ -1,9 +1,14 @@
 package systems.alexander.interiors.item;
 
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import systems.alexander.interiors.block.ModBlocks;
 
 public class ModCreativeModeTabs {
-    public static final CreativeModeTab INTERIORS_TAB = new CreativeTabStuff("interiors_tab");
-    public static void init() {
-    }
+    public static final CreativeModeTab INTERIORS_TAB = new CreativeModeTab("create_interiors_tab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModBlocks.RED_CHAIR_ICON.get());
+        }
+    };
 }
