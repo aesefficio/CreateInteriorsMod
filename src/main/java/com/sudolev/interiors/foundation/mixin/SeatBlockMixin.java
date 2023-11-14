0a4e48bb170bd298d565935c2cf28a35e9d794cb
levtus <i@alexander.systems>
2023-11-14 07:30:07 -0600

    edit SeatBlockMixin round 2

--- src/main/java/com/sudolev/interiors/foundation/mixin/SeatBlockMixin.java
+++ src/main/java/com/sudolev/interiors/foundation/mixin/SeatBlockMixin.java
@@ -3,12 +3,14 @@ package com.sudolev.interiors.foundation.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import com.simibubi.create.content.contraptions.components.actors.SeatBlock;
 import com.simibubi.create.content.contraptions.components.actors.SeatEntity;
+import com.sudolev.interiors.content.block.seat.BigChairBlock;
 import com.sudolev.interiors.content.entity.BigSeatEntity;
 
 import net.minecraft.core.BlockPos;
@@ -25,12 +27,12 @@ public abstract class SeatBlockMixin {
 			cir.setReturnValue(true);
 	}
 
-	// @Redirect(method = "sitDown", at = @At(value = "NEW", target = "(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)Lcom/simibubi/create/content/contraptions/actors/seat/SeatEntity;"), remap = false)
-	// private static SeatEntity createSeatEntity(Level world, BlockPos pos) {
-	// 	return world.getBlockState(pos).getBlock() instanceof BigChairBlock
-	//		   ? new BigSeatEntity(world, pos)
-	// 		   : new SeatEntity(world, pos);
-	// }
+	@Redirect(method = "sitDown", at = @At(value = "NEW", target = "(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)Lcom/simibubi/create/content/contraptions/components/actors/SeatEntity;"), remap = false)
+	private static SeatEntity createSeatEntity(Level world, BlockPos pos) {
+		return world.getBlockState(pos).getBlock() instanceof BigChairBlock
+				? new BigSeatEntity(world, pos)
+				: new SeatEntity(world, pos);
+	}
 
 	@Inject(method = "sitDown", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"), locals = LocalCapture.CAPTURE_FAILHARD)
 	private static void getY(Level world, BlockPos pos, Entity entity, CallbackInfo ci, SeatEntity seat) {
