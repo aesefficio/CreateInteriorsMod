7f7f2345011de2198701d4a22cc4c535a23f0362
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-02-27 10:31:32 -0500

    1.18.2 "port"

--- forge/src/main/java/com/sudolev/interiors/forge/CreateInteriorsForge.java
+++ forge/src/main/java/com/sudolev/interiors/forge/CreateInteriorsForge.java
@@ -12,10 +12,6 @@ public class CreateInteriorsForge {
 	public CreateInteriorsForge() {
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
-
-		forgeEventBus.register(Events.ClientModBusEvents.class);
-		modEventBus.addListener(Events.ClientModBusEvents::onLoadComplete);
-
 		CreateInteriors.init();
 		CreateInteriors.REGISTRATE.registerEventListeners(modEventBus);
 	}
