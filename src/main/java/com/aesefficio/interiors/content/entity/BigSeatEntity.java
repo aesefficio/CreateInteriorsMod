6586d3d861f209136cf94fc86d48539bd6d076a4
rdh <98863820+rhysdh540@users.noreply.github.com>
2025-12-14 19:33:54 -0500

    i'm a silly goober

--- src/main/java/com/aesefficio/interiors/content/entity/BigSeatEntity.java
+++ src/main/java/com/aesefficio/interiors/content/entity/BigSeatEntity.java
@@ -8,6 +8,7 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
